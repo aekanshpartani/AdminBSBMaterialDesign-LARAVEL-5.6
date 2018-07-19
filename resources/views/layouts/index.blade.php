08d4eab4882ef56b612e8bc55b553c3e6573a87c
aekansh partani <aekanshpartani@gmail.com>
2018-07-19 12:53:00 +0530

    1. Dashboard and index layout finished

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>@yield('title')</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+
+
+    @section('css')
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap Core Css -->
+        <link href="{{ URL::asset('bsb/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+        <!-- Waves Effect Css -->
+        <link href="{{ URL::asset('bsb/plugins/node-waves/waves.css')}}" rel="stylesheet" />
+
+        <!-- Animation Css -->
+        <link href="{{ URL::asset('bsb/plugins/animate-css/animate.css')}}" rel="stylesheet" />
+
+        <!-- Morris Chart Css-->
+        <link href="{{ URL::asset('bsb/plugins/morrisjs/morris.css')}}" rel="stylesheet" />
+
+        <!-- Custom Css -->
+        <link href="{{ URL::asset('bsb/css/style.css')}}" rel="stylesheet">
+
+        <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+        <link href="{{ URL::asset('bsb/css/themes/all-themes.css')}}" rel="stylesheet" />
+    @show
+
+    @yield('extra-css')
+
+
+
+
+</head>
+
+<body class="theme-red">
+
+@include('layouts.partials.loader')
+
+<!-- Overlay For Sidebars -->
+<div class="overlay"></div>
+<!-- #END# Overlay For Sidebars -->
+
+@include('layouts.partials.search-bar')
+
+
+@include('layouts.partials.top-navbar')
+
+
+<section>
+    @include('layouts.partials.left-sidebar')
+
+    @include('layouts.partials.right-sidebar')
+</section>
+
+<section class="content">
+    @yield('content')
+</section>
+
+@section('scripts')
+    <!-- Jquery Core Js -->
+    <script src="{{ URL::asset('bsb/plugins/jquery/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="{{ URL::asset('bsb/plugins/bootstrap/js/bootstrap.js')}}"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="{{ URL::asset('bsb/plugins/bootstrap-select/js/bootstrap-select.js')}}"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="{{ URL::asset('bsb/plugins/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="{{ URL::asset('bsb/plugins/node-waves/waves.js')}}"></script>
+
+@show
+@yield('extra-scripts')
+
+@section('bottom-scripts')
+    <!-- Custom Js -->
+    <script src="{{ URL::asset('bsb/js/admin.js')}}"></script>
+    <!-- Demo Js -->
+    <script src="{{ URL::asset('bsb/js/demo.js')}}"></script>
+@show
+
+</body>
+
+</html>
